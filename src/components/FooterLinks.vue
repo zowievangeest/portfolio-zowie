f21cfcb980b07afe3a507abc7dc2d56038c0f8e4
zowievangeest <zowie@zowievangeest.nl>
2018-05-07 13:18:27 +0200

    chore: add footer links component

---
+++ src/components/FooterLinks.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="footerLinks uk-position-bottom-center">
+    <vk-iconnav>
+        <vk-iconnav-item v-for="footerLink in footerLinks" :key="footerLink.id" v-bind:href="footerLink.url" v-bind:icon="footerLink.iconName"></vk-iconnav-item>
+    </vk-iconnav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterLinks',
+  data () {
+      return {
+         footerLinks: [
+             {url: 'https://www.linkedin.com/in/zowie-van-geest-9b76864b/', iconName: 'linkedin'},
+             {url: 'https://www.linkedin.com/in/zowie-van-geest-9b76864b/', iconName: 'github'},
+             {url: 'https://www.linkedin.com/in/zowie-van-geest-9b76864b/', iconName: 'facebook'}
+         ] 
+      }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
