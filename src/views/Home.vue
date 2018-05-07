dfc0924c2a53d6ef3d6e3800e781e5f5040b3935
zowievangeest <zowie@zowievangeest.nl>
2018-05-07 13:20:14 +0200

    refactor: architechture of home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <ComingSoon/>
+    <FooterLinks/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import ComingSoon from '@/components/ComingSoon.vue'
+import FooterLinks from '@/components/FooterLinks.vue'
 
 export default {
   name: 'home',
   components: {
-    ComingSoon
+    ComingSoon,
+    FooterLinks
   }
 }
 </script>
