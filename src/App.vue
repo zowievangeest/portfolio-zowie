27d622ffa0cec29db91789c1fa63d38faf7fa2cd
zowievangeest <zowie@zowievangeest.nl>
2018-05-07 14:27:16 +0200

    refactor: changes app structure

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
-  <div id="app">
+  <div id="app" class="background imgloaded">
+    <div class="content">
+			<div class="glitch">
+				<div class="glitch__img"></div>
+				<div class="glitch__img"></div>
+				<div class="glitch__img"></div>
+				<div class="glitch__img"></div>
+				<div class="glitch__img"></div>
+			</div>
+		</div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-
+// Custom Zowie
+@import "./assets/scss/main";
+@import "./assets/scss/colors";
+@import "./assets/scss/glitch";
 </style>
