a653dd904ce341cbffb5c24216a46af06b88371d
Zowie van Geest (from Travis CI) <zowie@zowievangeest.nl>
2018-08-02 07:50:16 +0000

    Deploy zowievangeest.nl to github.com/zowievangeest/portfolio-zowie.git:gh-pages

---
+++ precache-manifest.599f66e8921ce78819dd11c0467b5895.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e2efe39b40f95db57ca",
+    "url": "/js/chunk-vendors.7e2efe39.js"
+  },
+  {
+    "revision": "85d2fe294030bf8eab6e",
+    "url": "/js/app.85d2fe29.js"
+  },
+  {
+    "revision": "28610995ea93e01994c6fb26054137f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "424e445dc26ca872b926dec6dce9c2fa",
+    "url": "/img/logo-zowie-van-geest.424e445d.svg"
+  },
+  {
+    "revision": "8967cb03995a3e78cd3542786cc211e6",
+    "url": "/img/background-coming-soon-lq.8967cb03.jpg"
+  },
+  {
+    "revision": "7e2efe39b40f95db57ca",
+    "url": "/css/chunk-vendors.07b79b26.css"
+  },
+  {
+    "revision": "85d2fe294030bf8eab6e",
+    "url": "/css/app.a669e5bf.css"
+  }
+];
